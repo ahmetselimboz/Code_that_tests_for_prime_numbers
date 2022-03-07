0c26a81d727fb609d80e660fd7dd8e54f4f8f06d
ahmetselimboz <ahmetselimboz46@gmail.com>
2022-03-07 19:58:14 +0300

    Add project files.

---
+++ Prime Numbers/Prime Numbers.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int prime(int n) {
+	int i;
+	for (i = 2;i < n;i++) {
+
+		if (n % i == 0) {
+			return 0;
+		}
+	}
+
+
+}
+
+
+int main() {
+
+	int a;
+	printf("-----Code That Tests For Prime Numbers-----\n\n");
+	printf("--> Enter a number: \n");
+	scanf_s("%d", &a);
+	if (prime(a) == 0) {
+		printf("--> This is not a prime number.\n");
+	}
+	else {
+		printf("--> This is a prime number.\n");
+	}
+
+	system("pause");
+}
